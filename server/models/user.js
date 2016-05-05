70bc0b3b19d585817c99e82356b0b2df13a98021
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-05-05 18:43:56 -0400

    makes user schema

--- server/models/user.js
+++ server/models/user.js
@@ -5,5 +5,10 @@ var crypto = require('crypto');
 
 
 var userSchema = mongoose.Schema({
-  username: {type: Number}
-})
+  username: {type: Number, required: true, minlength: 10, maxlength: 10},
+  password: {type: String, required: true},
+  zipcode: {type: Number, required: true, minlength: 5, maxlength: 5},
+  temp_pref: {type: String, required: true},
+  is_admin: {type: Number, default: 0},
+  text_opt_in: {type: Number, default: 0}
+});
