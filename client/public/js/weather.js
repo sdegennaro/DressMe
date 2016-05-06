5381d7f69f7afce3242caa307e8e3d1aba444721
Michelle Varat <mvarat@gmail.com>
2016-05-06 08:44:13 -0400

    adds weather object

---
+++ client/public/js/weather.js
@@ -0,0 +1,20 @@
+var weatherRecs = {
+      sunny: {
+        minTemp: 80,
+        maxTemp: 90,
+        descriptiom: "Sunny",
+        url: "https://t1.ftcdn.net/jpg/00/55/12/64/160_F_55126421_UkxlTpL0Ost5O0UG1uYfPSY67yXvlpb6.jpg"
+      },
+      cloudy: {
+        minTemp: 60,
+        maxTemp: 80,
+        descriptiom: "Cloudy",
+        url: "http://blogs.worldbank.org/files/ic4d/01cloudy-t9953.jpg"
+      },
+      rain: {
+        minTemp: 60,
+        maxTemp: 80,
+        descriptiom: "Rain",
+        url: "http://www.vectorportal.com/img_novi/rainy3_7188.jpg"
+      }
+    }
