65afe1870e49fbc3e10fcb95f6fb0936dece9361
Michelle Varat <mvarat@gmail.com>
2016-05-08 22:10:06 -0400

    adds recomenndations routes and outfit scripts

--- server/routes/api/recommendations.js
+++ server/routes/api/recommendations.js
@@ -1,14 +1,26 @@
-var express                  = require('express'),
-    recommendationRouter     = express.Router(),
-    Recommendation           = require('../../models/recommendation.js');
+var express           = require('express'),
+    recRouter         = express.Router(),
+    Recommendation    = require('../../models/recommendation.js');
 
 // Create a new user
 
-recommendationRouter.get('/', function(req, res, next) {
+recRouter.get('/', function(req, res, next) {
   var degrees = req.query.degrees;
   var weather = req.query.description;
 
-  Recommendation.find({}, function (){
-    res.json({recommendation: response});  
+// Logic goes here to find the correct recommendation based upon degrees and weather
+  Recommendation.find({}, function (err, recResponse){
+    res.json({recommendation: recResponse});
   })
 })
+
+recRouter.post('/', function(req, res){
+  var recommendation = new Recommendation(req.body.recommendation);
+  console.log("load recommendation");
+  recommendation.save(function(err, recData){
+    console.log(err);
+    res.json(recData)
+  });
+});
+
+module.exports = recRouter;
