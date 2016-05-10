9f0fa110e2d7087dc3a696642298361f0e654527
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-05-10 16:45:36 -0400

    adds sendSMS function

--- client/public/js/text.js
+++ client/public/js/text.js
@@ -1,1 +1,15 @@
 console.log('so......text me maybe?');
+
+function sendText(message, toPhone){
+  $.ajax({
+    url: '/test',
+    type: "POST",
+    data: {
+      message: message,
+      to: toPhone,
+    },
+    success: function(data){
+      console.log(data);
+    }
+  })
+}
